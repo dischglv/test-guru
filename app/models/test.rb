b6853cafe17e530f532446b166bea0cee09304df
Diana Shchegoleva <dschegoleva@gmail.com>
2020-05-08 13:39:53 +0300

    Deleting commented line

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,5 @@
 class Test < ApplicationRecord
   def self.names_by_category(category_name)
-    # category_id = Category.find(title: category_name).id
     Test.joins('JOIN categories ON categories.id = tests.category_id')
         .where(categories: { title: category_name })
         .order(title: :desc)
