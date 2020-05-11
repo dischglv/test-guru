a907c4abef1cb553585515d466645760a5214ff4
Diana Shchegoleva <dschegoleva@gmail.com>
2020-05-11 15:29:59 +0300

    Adding associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,12 @@
 class Test < ApplicationRecord
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+  belongs_to :category
+  belongs_to :author, class_name: "User", foreign_key: "author_id"
+
   def self.names_by_category(category_name)
-    Test.joins('JOIN categories ON categories.id = tests.category_id')
+    Test.joins(:categories)
         .where(categories: { title: category_name })
         .order(title: :desc)
         .pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests, through: :results
+  has_many :created_tests, inverse_of: :author, class_name: "Test"
+
   def tests_by_level(level)
-    Test.joins('JOIN results ON results.test_id = tests.id')
+    Test.joins(:results)
         .where(results: { user_id: self.id }, level: level)
   end
 end
