aacfba25cdb2ea04f12e13f9b2b1c014cba87c0b
Diana Shchegoleva <dschegoleva@gmail.com>
2020-05-12 09:50:01 +0300

    Fixing associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 class Test < ApplicationRecord
-  has_many :questions
-  has_many :results
-  has_many :users, through: :results
   belongs_to :category
   belongs_to :author, class_name: "User", foreign_key: "author_id"
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results
 
   def self.names_by_category(category_name)
     Test.joins(:categories)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_many :results
+  has_many :results, dependent: :destroy
   has_many :tests, through: :results
-  has_many :created_tests, inverse_of: :author, class_name: "Test"
+  has_many :created_tests, inverse_of: :author, class_name: "Test",
+            dependent: :nullify, foreign_key: "author_id"
 
   def tests_by_level(level)
-    Test.joins(:results)
-        .where(results: { user_id: self.id }, level: level)
+    tests.where(level: level)
   end
 end
