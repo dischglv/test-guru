e6eecc6c671821f5a953cb9382b7c2617bb887d0
Diana Shchegoleva <dschegoleva@gmail.com>
2020-05-18 14:42:55 +0300

    Fixing

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :answer_text, presence: true
-  validate :max_number_of_answers
+  validate :max_number_of_answers, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def max_number_of_answers
-    errors.add(:base) if Answer.where(question_id: question_id).count >= 4
+    errors.add(:base) if question.answers.count >= 4
   end
 end
