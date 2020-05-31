c84b8041c30365954c40a33d1788fbd5005c68d8
Diana Shchegoleva <dschegoleva@gmail.com>
2020-05-31 13:03:02 +0300

    Fixing

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,13 +1,7 @@
 module TestPassagesHelper
 
   def test_progress(test_passage)
-    current_question_number = test_passage.test.number_of_questions - test_passage.count_remaining
-
-    "#{current_question_number}/#{test_passage.test.number_of_questions}"
-  end
-
-  def successful_pass?(test_passage)
-    test_passage.stats > 85
+    "#{test_passage.current_question_number}/#{test_passage.test.number_of_questions}"
   end
 
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,15 +3,18 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_update :before_update_set_next_question
+  before_validation :before_validation_set_current_question
+
+  def current_question_number
+    test.questions.order(:id).where('id <= :question_id', question_id: current_question.id).count
+  end
+
+  def success?
+    stats > 85
+  end
 
   def stats
     (correct_questions * 100 / test.number_of_questions).round
-  end
-
-  def count_remaining
-    remaining_questions.count
   end
 
   def completed?
@@ -27,20 +30,13 @@ class TestPassage < ApplicationRecord
   end
   
   private
-  
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
 
-  def before_update_set_next_question
+  def before_validation_set_current_question
     self.current_question = next_question
   end
   
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-    
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-    correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
   
   def correct_answers
@@ -52,7 +48,9 @@ class TestPassage < ApplicationRecord
   end
   
   def remaining_questions
-    test.questions.order(:id).where('id > :question_id', question_id: current_question.id)
+    current_question_id = current_question.nil? ? -1 : current_question.id
+    
+    test.questions.order(:id).where('id > :question_id', question_id: current_question_id)
   end
 
 end
