473d9507fbf3796eeca7161fc57e2ff7736e8f83
Diana Shchegoleva <dschegoleva@gmail.com>
2020-06-03 14:36:10 +0300

    Adding session

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test",
             dependent: :nullify, foreign_key: "author_id"
+
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
