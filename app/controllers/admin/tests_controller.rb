656eb9322d4aa185867499bb17d67fe4a85c6383
Diana Shchegoleva <dschegoleva@gmail.com>
2020-06-14 17:03:40 +0300

    Fixing

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -17,8 +17,7 @@ class Admin::TestsController < Admin::BaseController
   def edit; end
 
   def create
-    @test = Test.new(test_params)
-    @test.author = current_user
+    @test = current_user.created_tests.new(test_params)
 
     if @test.save
       redirect_to @test
@@ -38,11 +37,6 @@ class Admin::TestsController < Admin::BaseController
   def destroy
     @test.destroy
     redirect_to tests_path
-  end
-
-  def start
-    current_user.tests.push(@test)
-    redirect_to current_user.test_passage(@test)
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,16 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
+  has_many :created_tests, class_name: "Test",
+  dependent: :nullify, foreign_key: "author_id"
+  
   devise  :database_authenticatable,
           :registerable,
           :recoverable,
           :rememberable,
           :validatable
-
-  has_many :test_passages, dependent: :destroy
-  has_many :tests, through: :test_passages
-  has_many :created_tests, class_name: "Test",
-            dependent: :nullify, foreign_key: "author_id"
-
-  validates :email, presence: true,
-                    uniqueness: true,
-                    format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
@@ -30,15 +26,10 @@ class User < ApplicationRecord
   end
 
   def nickname
-    if first_name.present?
-      if last_name.present?
-        nickname = "#{last_name} #{first_name}"
-      else
-        nickname = first_name
-      end
-    else
-      nickname = email
-    end
+    return email unless first_name.present?
+    return first_name unless last_name.present?
+
+    "#{last_name} #{first_name}"    
   end
 
 end
